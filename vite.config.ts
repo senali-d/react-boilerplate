9b4befcfb9fe3a49ca984bd51dfa61af1bd4f068
Senali <senalidilumika@gmail.com>
2022-10-31 12:04:36 +0530

    Install and configure husky
      * npx husky-init && yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
 })
