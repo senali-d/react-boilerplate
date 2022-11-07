96650838573eef63a761027cb9d65eacfa1919a9
Senali <senalidilumika@gmail.com>
2022-11-07 16:23:06 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline ">Hello world!</h1>
+    <h1 className="text-3xl font-bold text-red-500 underline ">Hello world</h1>
   )
 }
 
